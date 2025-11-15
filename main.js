53311260226cba1a16423c1b54d64cdd58ebd02d
fennsen64 <fennsen64@users.noreply.github.com>
2025-11-15 14:29:18 +0100

    Update main.js
    Write of Pimatic Variables (Common Type error from Controller version 4.x upwards)
    
    see issue:
    https://github.com/ioBroker/ioBroker.pimatic/issues/20

--- main.js
+++ main.js
@@ -424,6 +424,7 @@ function syncVariables(variables, callback) {
                     name: variable.name,
                     read: true,
                     write: true,
+                    type: {},
                     role: 'pimatic-variable'
                 },
                 native: {
